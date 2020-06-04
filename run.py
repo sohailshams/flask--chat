2994296f14a2980a830e5ca0d41be8cb44b88e36
sohailshams <sohailshamas@yahoo.com>
2020-06-04 20:26:23 +0000

    added user form and chat variables to store user name

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randonstring123"
 messages = []
 
 
@@ -17,9 +18,15 @@ def get_all_messages():
     return "<br>".join(messages)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """ Main page with instructions """
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
     return render_template("index.html")
 
 
